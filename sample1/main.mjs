b8319a110e79dc4f6f47f24c2d89e221898d89d3
Umisyo <kusunokisouta@gmail.com>
2019-02-17 15:20:36 +0900

    初期座標の変更

--- sample1/main.mjs
+++ sample1/main.mjs
@@ -5,16 +5,16 @@
  * この関数が最初に実行される
  */
 async function main() {
-  // 地図を初期化
-  const map = new Y.Map('map');
-  const zoomLevel = 17;
-  const lng = 35.68227528;
-  const lat = 139.73310240;
-  map.drawMap(new Y.LatLng(lng, lat), zoomLevel, Y.LayerSetId.NORMAL);
+    // 地図を初期化
+    const map = new Y.Map('map');
+    const zoomLevel = 17;
+    const lng = 34.7038830;
+    const lat = 135.4950516;
+    map.drawMap(new Y.LatLng(lng, lat), zoomLevel, Y.LayerSetId.NORMAL);
 
-  // コントロールの追加
-  const sliderZoomControl = new Y.SliderZoomControlVertical();
-  map.addControl(sliderZoomControl);
+    // コントロールの追加
+    const sliderZoomControl = new Y.SliderZoomControlVertical();
+    map.addControl(sliderZoomControl);
 }
 
 main();
