b4fe489ae6053be6f4708a97359e179471d25d5b
Umisyo <kusunokisouta@gmail.com>
2019-02-17 14:46:22 +0900

    first commit

--- sample2/main.mjs
+++ sample2/main.mjs
@@ -24,17 +24,17 @@
  * @return {Feature[]} 位置情報の一覧
  */
 async function requestGeoCoderAPI(query) {
-  const response = await axios({
-    url: 'https://{あなたのApigeeURL}/geocode/V1/geoCoder',
-    params: {
-      query,
-      output: 'json',
-    },
-  });
-  const { data } = response;
-  // APIから受け取った内容をコンソールに表示
-  console.log(JSON.stringify(data, undefined, 2));
-  return data.ResultInfo.Count > 0 ? data.Feature : [];
+    const response = await axios({
+        url: 'https://kusunokisouta-eval-test.apigee.net/yahoo_map/geocode/V1/geoCoder',
+        params: {
+            query,
+            output: 'json',
+        },
+    });
+    const { data } = response;
+    // APIから受け取った内容をコンソールに表示
+    console.log(JSON.stringify(data, undefined, 2));
+    return data.ResultInfo.Count > 0 ? data.Feature : [];
 }
 
 /**
@@ -43,9 +43,9 @@ async function requestGeoCoderAPI(query) {
  * @return {object} 緯度(lat)経度(lng)
  */
 function parseCoordinates(str) {
-  // 入力Coordinatesは"経度,緯度"のフォーマットで来る
-  const [lng, lat] = str.trim().split(',');
-  return { lng: Number(lng), lat: Number(lat) };
+    // 入力Coordinatesは"経度,緯度"のフォーマットで来る
+    const [lng, lat] = str.trim().split(',');
+    return { lng: Number(lng), lat: Number(lat) };
 }
 
 /**
@@ -53,36 +53,36 @@ function parseCoordinates(str) {
  * この関数が最初に実行される
  */
 async function main() {
-  // 地図を初期化
-  const map = new Y.Map('map');
-  const zoomLevel = 17;
-  const lng = 35.68227528;
-  const lat = 139.73310240;
-  map.drawMap(new Y.LatLng(lng, lat), zoomLevel, Y.LayerSetId.NORMAL);
+    // 地図を初期化
+    const map = new Y.Map('map');
+    const zoomLevel = 17;
+    const lng = 35.68227528;
+    const lat = 139.73310240;
+    map.drawMap(new Y.LatLng(lng, lat), zoomLevel, Y.LayerSetId.NORMAL);
 
-  // コントロールの追加
-  const sliderZoomControl = new Y.SliderZoomControlVertical();
-  map.addControl(sliderZoomControl);
+    // コントロールの追加
+    const sliderZoomControl = new Y.SliderZoomControlVertical();
+    map.addControl(sliderZoomControl);
 
-  // 「検索する」ボタンを押したときのアクションを設定する
-  document.getElementById('form').onsubmit = () => {
-    /** @type {string} テキストボックスの入力内容 */
-    const query = document.getElementById('query').value;
-    // ジオコーダAPIの検索結果を反映させる
-    (async () => {
-      const geoFeatures = await requestGeoCoderAPI(query);
-      const resultText = document.getElementById('result-text');
-      if (geoFeatures.length > 0) {
-        const [first] = geoFeatures;
-        const lct = parseCoordinates(first.Geometry.Coordinates);
-        resultText.textContent = `場所: ${first.Name}, 緯度: ${lct.lat}, 経度: ${lct.lng}`;
-      } else {
-        resultText.textContent = '正しい住所を入力してください';
-      }
-    })();
-    // 戻り値をfalseにして、formのsubmitが常に成功しないようにする
-    return false;
-  };
+    // 「検索する」ボタンを押したときのアクションを設定する
+    document.getElementById('form').onsubmit = () => {
+        /** @type {string} テキストボックスの入力内容 */
+        const query = document.getElementById('query').value;
+        // ジオコーダAPIの検索結果を反映させる
+        (async() => {
+            const geoFeatures = await requestGeoCoderAPI(query);
+            const resultText = document.getElementById('result-text');
+            if (geoFeatures.length > 0) {
+                const [first] = geoFeatures;
+                const lct = parseCoordinates(first.Geometry.Coordinates);
+                resultText.textContent = `場所: ${first.Name}, 緯度: ${lct.lat}, 経度: ${lct.lng}`;
+            } else {
+                resultText.textContent = '正しい住所を入力してください';
+            }
+        })();
+        // 戻り値をfalseにして、formのsubmitが常に成功しないようにする
+        return false;
+    };
 }
 
 main();
